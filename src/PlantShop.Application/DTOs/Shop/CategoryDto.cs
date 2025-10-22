9ba55bf26cda10433d5fbe105f065ebfd2010ca1
Pedro Verde <pedroverde@gmail.com>
2025-10-23 00:44:14 +0100

    Add first Dto

---
+++ src/PlantShop.Application/DTOs/Shop/CategoryDto.cs
@@ -0,0 +1,8 @@
+﻿namespace PlantShop.Application.DTOs.Shop;
+
+public  class CategoryDto
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string? Description { get; set; }
+}
