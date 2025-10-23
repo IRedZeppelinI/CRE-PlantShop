07e09f6b860c08eb69483edb8464ba01dfe3c855
Pedro Verde <pedroverde@gmail.com>
2025-10-23 01:13:59 +0100

    Add Category repo interface and implementation for two tasks

---
+++ src/PlantShop.Application/Interfaces/Persistence/ICategoryRepository.cs
@@ -0,0 +1,13 @@
+﻿using PlantShop.Application.DTOs.Shop;
+using PlantShop.Domain.Entities.Shop;
+
+namespace PlantShop.Application.Interfaces.Persistence;
+
+public interface ICategoryRepository
+{
+    Task<IEnumerable<Category>> GetAllAsync(CancellationToken cancellationToken = default);
+    Task<Category?> GetByIdAsync(int id, CancellationToken cancellationToken = default);
+    Task AddAsync(Category category, CancellationToken cancellationToken = default);
+    Task UpdateAsync(Category category, CancellationToken cancellationToken = default);
+    Task DeleteAsync(Category category, CancellationToken cancellationToken = default);
+}
---
+++ src/PlantShop.Infrastructure/Persistence/Repositories/CategoryRepository.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PlantShop.Application.Interfaces.Persistence;
+using PlantShop.Domain.Entities.Shop;
+
+namespace PlantShop.Infrastructure.Persistence.Repositories;
+
+public class CategoryRepository : ICategoryRepository
+{
+    private readonly ApplicationDbContext _context;
+
+    public CategoryRepository(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+
+    public async Task AddAsync(Category category, CancellationToken cancellationToken = default)
+    {
+        await _context.Categories.AddAsync(category, cancellationToken);
+    }
+
+    public Task DeleteAsync(Category category, CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<IEnumerable<Category>> GetAllAsync(CancellationToken cancellationToken = default)
+    {
+        return await _context.Categories
+                .AsNoTracking()
+                .ToListAsync(cancellationToken);
+    }
+
+    public Task<Category?> GetByIdAsync(int id, CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task UpdateAsync(Category category, CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+}
