56ca45e5c5881dacdb2bc80f4fb797872407af79
Pedro Verde <pedroverde@gmail.com>
2025-10-29 20:07:29 +0000

    Change from blazor to MVC on PlanShop.WebUI

--- src/PlantShop.WebUI/Program.cs
+++ src/PlantShop.WebUI/Program.cs
@@ -1,7 +1,6 @@
-using PlantShop.Application;
 using PlantShop.Infrastructure;
 using PlantShop.Infrastructure.Persistence;
-using PlantShop.WebUI.Components;
+using PlantShop.Application;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,19 +8,18 @@ builder.Services.AddInfrastructureServices(builder.Configuration);
 builder.Services.AddApplicationServices();
 
 // Add services to the container.
-builder.Services.AddRazorComponents()
-    .AddInteractiveServerComponents();
+builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
-{    
+{
     using (var scope = app.Services.CreateScope())
     {
         var services = scope.ServiceProvider;
         try
         {
-            var context = services.GetRequiredService<ApplicationDbContext>();                        
+            var context = services.GetRequiredService<ApplicationDbContext>();
 
             await DataSeeder.SeedAsync(context);
         }
@@ -37,18 +35,22 @@ if (app.Environment.IsDevelopment())
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
+app.UseRouting();
 
-
-app.UseAntiforgery();
+app.UseAuthorization();
 
 app.MapStaticAssets();
-app.MapRazorComponents<App>()
-    .AddInteractiveServerRenderMode();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}")
+    .WithStaticAssets();
+
 
 app.Run();
